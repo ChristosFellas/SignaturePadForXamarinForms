69c53a33b303cf94fbe4ec7d4116b4d1fb51f087
Christos Fellas <christosfellas@hotmail.co.uk>
2017-11-08 00:21:25 +0000

    Initial Commit

---
+++ SigniturePadDemo/SigniturePadDemo/SignaturePadPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xamarin.Forms;
+
+namespace SigniturePadDemo
+{
+    public partial class SignaturePadPage : ContentPage
+    {
+        public SignaturePadPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            Stream image = await PadView1.GetImageStreamAsync(SignaturePad.Forms.SignatureImageFormat.Jpeg);
+        }
+
+        private void ClearButton_Clicked(object sender, System.EventArgs e)
+        {
+            PadView1.Clear();
+        }
+    }
+}
